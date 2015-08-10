3eff4f34339a89ab90afc234731a2cbee317628c
Taras Shkodenko <podlom@gmail.com>
2015-08-10 06:05:52 -0700

    Updated calc_avg_httpd_size.php

--- calc_avg_httpd_size.php
+++ calc_avg_httpd_size.php
@@ -7,52 +7,24 @@
  * 
  *
  * @author Shkodenko V. Taras (taras -at- shkodenko.com)
- * @requires web server Apache (httpd)
+ * @requires ps, gawk, web server Apache process name (httpd|apache2)
  *
  */
 
+// Apache 2 process name depends on OS e.g.: Red Hat, CentOs: httpd; Debian, Ubuntu: apache2
 $httpd_name = "httpd";
 //
-$avg_httpd_size_data_cmd = "ps auxwww |grep '{$httpd_name}' |grep -v 'grep' |grep -v '" . __FILE__ . "' |awk '{print \$5/1024}'";
-echo __FILE__ . ' +' . __LINE__ . ' running command: ' . $avg_httpd_size_data_cmd . PHP_EOL;
+$cmd1 = "ps -C '{$httpd_name}' -O rss | gawk '{ count ++; sum += $2 }; END {count --; print \"Number of processes =\",count; print \"Average memory usage per process =\", sum/1024/count , \"MB\"; print \"Total memory usage =\", sum/1024, \"MB\" ;};'";
+// 
+echo __FILE__ . ' +' . __LINE__ . ' running command: ' . $cmd1 . PHP_EOL;
 
-ob_start();
-passthru($avg_httpd_size_data_cmd, $res1);
-$cmd_output = ob_get_contents();
-ob_end_clean();
+exec($cmd1, $cmd_output, $res1);
 
 // echo __FILE__ . ' +' . __LINE__ . ' command result: ' . var_export($res1, 1) . PHP_EOL;
 // echo __FILE__ . ' +' . __LINE__ . ' command output: ' . PHP_EOL . var_export($cmd_output, 1) . PHP_EOL;
 
-$a0 = array();
-if( !empty($cmd_output) ) {
-    preg_match_all('|\d+|m', $cmd_output, $a1);
-    if( is_array($a1) && (count($a1) > 0) ) {
-        foreach($a1 as $a2) {
-            $a0[] = $a2;
-        }
+if ( !empty($cmd_output) ) {
+    foreach ($cmd_output as $rep_line) {
+        echo $rep_line . PHP_EOL;
     }
-    // echo var_export($a0[0], 1) . PHP_EOL;
-    $avg_val = calculate_average($a0[0]);
-    echo 'Average value memory, Mb: ' . intval($avg_val) . PHP_EOL;
 }
-
-
-/**
- * Float calculate_average(Array $arr): calculates avarage httpd process size, Mb
- */
-function calculate_average($arr) {
-    // total numbers in array
-    $count = count($arr);
-    echo 'Number of Apache children: ' . $count . PHP_EOL;
-    $total = 0;
-    foreach ($arr as $value) {
-        // total value of array numbers
-        $total = $total + $value;
-    }
-    echo 'Total memory usage by Apache children, Mb: ' . $total . PHP_EOL;
-    // get average value
-    $average = ($total/$count);
-    return $average;
-}
-
