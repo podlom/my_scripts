7987548b298d119601f7e8372a59b25a7b5d4fc7
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-06-05 02:06:23 +0300

    Update make_git_archive.cmd

--- make_git_archive.cmd
+++ make_git_archive.cmd
@@ -4,7 +4,13 @@
 @rem
 @rem @see: https://stackoverflow.com/questions/203090/how-do-i-get-current-datetime-on-the-windows-command-line-in-a-suitable-format
 @rem
+cd /d "C:\os\OSPanel\domains\pw\testing"
+@rem
+dir "C:\os\OSPanel\domains\pw\test-backups\"
+@rem
 For /f "tokens=1-3 delims=. " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
-cd /d "C:\os\OSPanel\domains\pw\testing"
 git archive HEAD --format=zip -o "C:\os\OSPanel\domains\pw\test-backups\testing_cf-phalcon_%mydate%_%mytime%.zip"
+@rem
+dir "C:\os\OSPanel\domains\pw\test-backups\"
+pause
