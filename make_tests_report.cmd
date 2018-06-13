98383ae7e6eb688d7f578eb92d63d48c5bd68601
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-06-13 21:15:13 +0300

    Update make_tests_report.cmd

--- make_tests_report.cmd
+++ make_tests_report.cmd
@@ -1,17 +1,25 @@
-@echo off
+@rem echo off
 @rem
 @rem @author Shkodenko V. Taras <taras@shkodenko.com>
 @rem
 @rem Run codepation acceptance tests and generate report in HTML format
 @rem
-cd /d "C:\os\OSPanel\domains\pw\testing"
+@set testing_dir=C:\os\OSPanel\domains\pw\testing
+echo "Set testing dir to: %testing_dir%"
+@rem pause
+@set reports_dir=C:\os\OSPanel\domains\pw\test-reports\
+echo "Set reports dir to: %reports_dir%"
+@rem pause
+dir %reports_dir%
 @rem
-dir "C:\os\OSPanel\domains\pw\test-reports\"
+@For /f "tokens=1-3 delims=. " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
+@For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
 @rem
-For /f "tokens=1-3 delims=. " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
-@rem
-vendor\bin\codecept.bat run acceptance --steps --html="C:\os\OSPanel\domains\pw\test-reports\report_%mydate%_%mytime%.html"
-@rem
-dir "C:\os\OSPanel\domains\pw\test-reports\"
+cd /d %testing_dir%
+vendor\bin\codecept.bat run acceptance --steps --no-colors --html="%reports_dir%report_%mydate%_%mytime%.html"
+pause
+@rem Clean-up mess with report file names containing spaces
+cd /d %reports_dir%
+php fix-test-report-names.php
+dir %reports_dir%
 pause
