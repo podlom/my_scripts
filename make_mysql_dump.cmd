fc5492779f7522549afacb8929dea62c6faf02f4
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-06-13 21:13:54 +0300

    Update make_mysql_dump.cmd

--- make_mysql_dump.cmd
+++ make_mysql_dump.cmd
@@ -10,5 +10,6 @@ For /f "tokens=1-3 delims=." %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
 @rem
 "C:\os\OSPanel\modules\database\MySQL-5.7-x64\bin\mysqldump.exe" -uroot cf-phalcon-clean > "C:\os\OSPanel\domains\pw\_db_backups\cf-phalcon-clean_taras_%mydate%_%mytime%.sql"
+php fix-dump-names.php
 dir "C:\os\OSPanel\domains\pw\_db_backups"
 pause
